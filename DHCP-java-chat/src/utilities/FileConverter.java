48140b749b61cfd256ec69a1a871993bf7b1e619
Hung Cuong <hungcuongnd@gmail.com>
2017-12-09 23:59:49 +0700

    re-Initial commitcreate project with no virus

---
+++ DHCP-java-chat/src/utilities/FileConverter.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utilities;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Base64;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.ImageIcon;
+
+/**
+ * A file converter
+ *
+ * @author Tran Cuong
+ */
+public final class FileConverter {
+
+    /**
+     * Convert a file to Base64 string
+     *
+     * @param url a path of a file what converted to string
+     * @return a string
+     */
+    public static String fileToString(String url) {
+        if (url == null || url.isEmpty()) {
+            return null;
+        }
+
+        Path path = Paths.get(url);
+        try {
+            byte[] bytes = Files.readAllBytes(path);
+            return Base64.getEncoder().encodeToString(bytes);
+        } catch (IOException ex) {
+            Logger.getLogger(FileConverter.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    /**
+     * Convert a string to ImageIcon (swing)
+     *
+     * @param encode a string encoded from a file
+     * @return a ImageIcon
+     */
+    public static ImageIcon stringToImage(String encode) {
+        if (encode == null || encode.isEmpty()) {
+            return null;
+        }
+        
+        byte[] decode = Base64.getDecoder().decode(encode);
+        return new ImageIcon(decode);
+    }
+}
---
+++ DHCP-java-chat/src/utilities/Request.java
@@ -0,0 +1,98 @@
+package utilities;
+
+import utilities.Content;
+
+public class Request {
+
+    private int type;
+    private String fromUser;
+    private String password;
+    private String toUser;
+    private String toGroup;
+    private Content content;
+    public String id;
+
+    private boolean login = true;
+    private String avatar;
+
+    public Request() {
+
+    }
+
+    public Request(int type, String fromUser, String toUser) {
+        this.type = type;
+        this.fromUser = fromUser;
+
+        this.toUser = toUser;
+    }
+
+    public String getFromUser() {
+        return fromUser;
+    }
+
+    public void setFromUser(String fromUser) {
+        this.fromUser = fromUser;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getToUser() {
+        return toUser;
+    }
+
+    public void setToUser(String toUser) {
+        this.toUser = toUser;
+    }
+
+    public String getToGroup() {
+        return toGroup;
+    }
+
+    public void setToGroup(String toGroup) {
+        this.toGroup = toGroup;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public Content getContent() {
+        return content;
+    }
+
+    public void setContent(Content content) {
+        this.content = content;
+    }
+
+    public boolean isLogin() {
+        return login;
+    }
+
+    public void setLogin(boolean login) {
+        this.login = login;
+    }
+    
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    @Override
+    public String toString() {
+        return "Request{" + "type=" + type + ", fromUser=" + fromUser + ", password=" + password + ", toUser=" + toUser + ", toGroup=" + toGroup + ", content=" + content + ", login=" + login + '}';
+    }
+
+}
