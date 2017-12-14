a1bbf80173184c7fe154e743aabdcd78eb0a93eb
Tri Phuong <phuonglt32@wru.vn>
2017-12-14 21:55:54 +0700

    Sua lai ham create user.

--- DHCP-java-chat/src/database/DAO/ListfriendDAO.java
+++ DHCP-java-chat/src/database/DAO/ListfriendDAO.java
@@ -1,4 +1,4 @@
- /*
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -15,12 +15,16 @@ import javax.persistence.EntityManager;
  * @author Administrator
  */
 public class ListfriendDAO {
-    
+
     public EntityManager em = project2Utility.createConnect();
-    
-    public List<Tbluser> getListFriend(String username){
-        List<Tbluser> userfriend = em.createQuery("SELECT u from Tbluser u JOIN Tblfriend f WHERE (u.userName = f.tblfriendPK.userName2 AND f.tblfriendPK.userName1 = '"+ username+"')"
-                +" OR (u.userName = f.tblfriendPK.userName1 AND f.tblfriendPK.userName2 = '"+ username+"') ", Tbluser.class).getResultList();
-        return userfriend;
+
+    public List<Tbluser> getListFriend(String username) {
+        try {
+            List<Tbluser> userfriend = em.createQuery("SELECT u from Tbluser u JOIN Tblfriend f WHERE (u.userName = f.tblfriendPK.userName2 AND f.tblfriendPK.userName1 = '" + username + "')"
+                    + " OR (u.userName = f.tblfriendPK.userName1 AND f.tblfriendPK.userName2 = '" + username + "') ", Tbluser.class).getResultList();
+            return userfriend;
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
